4cd17618e77c3768573549c6d9d72763c4ef87d2
dwyfrequency <jdwy215@gmail.com>
2019-07-13 20:14:37 -0400

    added notes on  integration testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { add, total } from './App';
 
-//  Unit tests
+//  Unit tests - when we are just testing a specific func or component
 test('should sum values', () => {
   expect(add(1, 2)).toBe(3);
 });
@@ -9,7 +9,7 @@ test('should add correctly', () => {
   expect(add(1, 7)).not.toBe(4);
 });
 
-// integration tests
+// integration tests - when we are testing how it integrates with other code
 test('should ', () => {
   expect(total(1, 7)).toBe('$8');
 });
