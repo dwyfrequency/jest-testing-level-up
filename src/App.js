c217bd88bde1d9352ec2c3aba29e5d8e18381f0e
dwyfrequency <jdwy215@gmail.com>
2019-07-13 20:12:07 -0400

    notes on tests

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
 export const add = (x, y) => x + y;
+
+export const total = (subtotal, shipping) => {
+  return '$' + add(subtotal, shipping);
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import { add } from './App';
+import { add, total } from './App';
 
+//  Unit tests
 test('should sum values', () => {
   expect(add(1, 2)).toBe(3);
 });
 
 test('should add correctly', () => {
   expect(add(1, 7)).not.toBe(4);
+});
+
+// integration tests
+test('should ', () => {
+  expect(total(1, 7)).toBe('$8');
 });
