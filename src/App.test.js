2e09193780a7a18cb8da2553fc1c76d656de269e
dwyfrequency <jdwy215@gmail.com>
2019-07-13 20:25:02 -0400

    adding basic notes

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,19 @@ test('should add correctly', () => {
 });
 
 // integration tests - when we are testing how it integrates with other code
-test('should ', () => {
+test('should return summed value with dollar sign', () => {
   expect(total(1, 7)).toBe('$8');
+});
+
+// Mock Functions - a fake func that gives you a defined result
+// we can even load functions and turn them into mocks ie. testing with db or api call without
+// actually hitting the api
+const plus = jest.fn();
+test('should be called twice and without params', () => {
+  plus(1);
+  // if values are coming in from other areas we can validate with this
+  expect(plus).toHaveBeenCalledWith(1);
+  plus();
+  // can register how many times a func has been called
+  expect(plus).toHaveBeenCalledTimes(2);
 });
